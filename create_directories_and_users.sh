6d97095f7cd2b78bb5c0b7cfb99ea1580d3e9d5d
Joachim Hereth <jhereth@daten-kieker.de>
2018-03-26 09:01:21 +0200

    Fixup Syntax error.

--- create_directories_and_users.sh
+++ create_directories_and_users.sh
@@ -6,7 +6,7 @@
 
 set -eux
 
-$BLOG = $1
+BLOG=$1
 
 addgroup www-$BLOG # create group for www-data and new user
 useradd -G www-$BLOG -N -p '*' -c "$BLOG deploy user,,," $BLOG # deploy user without password
