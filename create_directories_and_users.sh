eb46d6512a1e67a0dba517cd3413acd80136bf22
Joachim Hereth <joachim.hereth@numberfour.eu>
2018-03-26 08:57:47 +0200

    Fixup

--- create_directories_and_users.sh
+++ create_directories_and_users.sh
@@ -1,11 +1,12 @@
-!# /usr/bin/env bash
+#! /usr/bin/env bash
 
 # Usage
 # sudo ./create_directories_and_users.sh <blogname>
 
-$BLOG = $1
 
 set -eux
+
+$BLOG = $1
 
 addgroup www-$BLOG # create group for www-data and new user
 useradd -G www-$BLOG -N -p '*' -c "$BLOG deploy user,,," $BLOG # deploy user without password
