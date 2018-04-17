4c09cf3c78b93289154deb9535895f3f2fad85c8
Joachim Hereth <jhereth@daten-kieker.de>
2018-04-17 10:04:26 +0200

    Convenience script for deploy user.

--- deploy_user_ssh.sh
+++ deploy_user_ssh.sh
@@ -1,8 +1,24 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 # Usage:
 # 1. First sudo to deploy user `sudo su - www-$BLOG`
 # 2. run `./deploy_user_ssh.sh $BLOG
 
-ssh-keygen -t rsa -b 2048 -C "$BLOG deploy user" -f .ssh/id_${BLOG}_deploy
-cat .ssh/id_${BLOG}_deploy.pub >> .ssh/authorized_keys
+set -eu
+
+BLOG=$1
+
+
+mkdir -p $HOME/.ssh
+ssh-keygen -t rsa -b 2048 -C "$BLOG deploy user" -f $HOME/.ssh/id_${BLOG}_deploy
+cat $HOME/.ssh/id_${BLOG}_deploy.pub >> $HOME/.ssh/authorized_keys
+
+# Keys have to be copied to localhost
+echo "Save keys on local machine"
+echo "echo '"
+cat $HOME/.ssh/id_${BLOG}_deploy
+echo "' > id_${BLOG}_deploy"
+echo "echo '"
+cat $HOME/.ssh/id_${BLOG}_deploy.pub
+echo "' > id_${BLOG}_deploy.pub"
+
